2c9a86f3e726d0deb91e2b0a9be75a6b12a13a69
Mark Skelton <mdskelton99@gmail.com>
2020-04-05 16:08:52 -0500

    Prettier

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,5 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react";
 
-interface ContainerProps { }
-
-const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
-    <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
-};
-
-export default ExploreContainer;
+export function Hi() {
+  return <div>hi</div>;
+}
